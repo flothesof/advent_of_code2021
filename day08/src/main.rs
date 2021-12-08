7e7bc0f3fd92666bfe9abe72cd8aa7667d68f201
flothesof <florian.s.lebourdais@gmail.com>
2021-12-08 07:53:55 +0100

    day08 part1

---
+++ day08/src/main.rs
@@ -0,0 +1,22 @@
+use std::cmp;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+fn main() {
+    let f = File::open("input").expect("Unable to open file");
+    let f = BufReader::new(f);
+    let unique: Vec<i32> = vec![2, 3, 4, 7];
+    let mut sum = 0;
+    for line in f.lines() {
+        let line = line.expect("Unable to read line");
+        let mut line = line.split("|");
+        line.next();
+        let digits: Vec<&str> = line.next().unwrap().split(" ").collect();
+        for digit in digits {
+            if unique.contains(&(digit.to_string().len() as i32)) {
+                sum += 1;
+            };
+        }
+    }
+    println!("part 1: {}", sum);
+}
